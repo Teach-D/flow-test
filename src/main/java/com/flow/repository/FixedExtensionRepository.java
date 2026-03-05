3bebb7db1b388aed81b2088ef5d4e30850801e8e
unknown <wkadht0619@gmail.com>
2026-03-05 21:56:49 +0900

    feat: FixedExtensionRepository 구현 #1

---
+++ src/main/java/com/flow/repository/FixedExtensionRepository.java
@@ -0,0 +1,7 @@
+package com.flow.repository;
+
+import com.flow.entity.FixedExtension;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FixedExtensionRepository extends JpaRepository<FixedExtension, String> {
+}
