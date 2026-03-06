ea87a39b137ac492e108355071782a8d1a3f835c
unknown <wkadht0619@gmail.com>
2026-03-06 11:26:38 +0900

    유효성 메시지 개선 및 커스텀 확장자 대소문자 구분 처리 #23

--- src/main/java/com/flow/dto/CustomExtensionRequest.java
+++ src/main/java/com/flow/dto/CustomExtensionRequest.java
@@ -10,8 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CustomExtensionRequest {
 
-    @NotBlank
-    @Size(max = 20)
-    @Pattern(regexp = "^[a-zA-Z0-9]+$")
+    @NotBlank(message = "확장자를 입력해주세요.")
+    @Size(max = 20, message = "최대 20자까지 입력 가능합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "영문자와 숫자만 입력 가능합니다.")
     private String name;
 }
